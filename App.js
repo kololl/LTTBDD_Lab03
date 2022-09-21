3f0942f6dda4f3395a49b8da05bc3a4e55b6c265
kololl <tieuhivong7201@gmail.com>
2022-09-22 01:27:56 +0700

    update

--- App.js
+++ App.js
@@ -1,20 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { 
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ImageBackground,
+  TouchableOpacity,
+  Button,
+  Stack,
+   } from "react-native";
+ 
+export default function App(){
+  return(
+      <View style={style.container}>
+  <ImageBackground 
+  source ={ require('./image/background.png')}
+  style={{
+    height:'100%',
+    width:'100%',
+  }}
+>
+    
+    <Image style={{
+      height:142,
+      width:142,
+      paddingRight:30,
+      margin:120,
+    }}
+      source={require('./image/circle.png')}/>
+  
+   <View >
+    <Text style={{fontSize:30,
+                  color:'#000000',
+                  fontWeight:'bold',
+                  textAlign:'center',
+                  marginTop:-50}}> GROW YOUR BUSINESS</Text>
+    <Text style={{fontSize:20,
+                  color:'#000000',
+                  textAlign:'center'
+                  }}> We will help you to grow your business using
+                  online server</Text> 
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+   <Button style={{width:'152',
+                  height:45,
+                  top:471,
+                  margin:30,
+                  textAlign:'#E3C000'
+                  }} title="LOGIN" />  
+    <Text style={{fontSize:30,
+                  color:'#000000',
+                  fontWeight:'bold',
+                  textAlign:'center',
+                  marginTop:100}}> HOW WE WORK?</Text>
+    
+  </View>
+  
+  
+
+  </ImageBackground> 
+  
+  
+  </View>
+  
   );
 }
+const style=StyleSheet.create({
+container:{
+  justifyContent:'center',
+  alignItems:'center',
+  flex:1,
+}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+})
+
